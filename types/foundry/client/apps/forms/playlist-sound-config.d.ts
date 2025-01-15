9b04940f15fa0aaa59e9592d95a9c1ef1b7d03a1
DFreds <dfreds08@gmail.com>
2025-01-14 19:05:44 -0500

    Initial commit

---
+++ types/foundry/client/apps/forms/playlist-sound-config.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Playlist Sound Configuration Sheet
+ *
+ * @param sound     The sound object being configured
+ * @param options   Additional application rendering options
+ */
+declare class PlaylistSoundConfig extends FormApplication {
+    protected override _updateObject(
+        event: Event,
+        formData: Record<string, unknown>,
+    ): Promise<void>;
+}
