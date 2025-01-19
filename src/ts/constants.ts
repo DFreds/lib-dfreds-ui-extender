9908a8f81b6f782947261fadbf106deeb7ecd5c7
Derek Fredrickson <dfreds08@gmail.com>
2025-01-18 22:52:28 -0500

    Remove sample and debug constant

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,5 +1,3 @@
 import moduleData from "@static/module.json" assert { type: "json" };
 
-export const DEBUG = false;
-
 export const MODULE_ID = moduleData.id;
--- src/ts/hooks/init.ts
+++ src/ts/hooks/init.ts
@@ -1,11 +1,9 @@
-import { DEBUG } from "../constants.ts";
 import { UiExtender } from "../ui-extender.ts";
 import { Listener } from "./index.ts";
 
 const Init: Listener = {
     listen(): void {
         Hooks.once("init", () => {
-            CONFIG.debug.hooks = DEBUG;
             UiExtender.init();
         });
     },
