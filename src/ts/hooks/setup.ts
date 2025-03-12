073525cfdfcce7cfb94399ec98c308cb0cc8469d
Derek Fredrickson <dfreds08@gmail.com>
2025-03-12 13:10:06 -0400

    Add debug hooks if dev mode

--- src/ts/hooks/setup.ts
+++ src/ts/hooks/setup.ts
@@ -4,6 +4,11 @@ import { Listener } from "./index.ts";
 const Setup: Listener = {
     listen(): void {
         Hooks.once("setup", () => {
+            if (BUILD_MODE === "development") {
+                // Setup debug mode
+                CONFIG.debug.hooks = true;
+            }
+
             UiExtender.setup();
         });
     },
