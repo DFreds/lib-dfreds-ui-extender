56a0e97a8a8e2c4f333ac0fa631b0e2a259ec702
Derek Fredrickson <dfreds08@gmail.com>
2025-03-30 00:10:35 -0400

    Expose own interfaces rather than use the pf2e types one

--- src/ts/scene-controls/scene-control-input.ts
+++ src/ts/scene-controls/scene-control-input.ts
@@ -8,8 +8,8 @@ interface SceneControlInput {
      * The name of the control layer
      */
     name:
-        | "token"
-        | "measure"
+        | "tokens"
+        | "templates"
         | "tiles"
         | "drawings"
         | "walls"
@@ -17,11 +17,6 @@ interface SceneControlInput {
         | "sounds"
         | "regions"
         | "notes";
-
-    /**
-     * The position to put the button. If no number is given, it will append it to the end
-     */
-    position?: number;
 
     /**
      * The predicate to determine if the control should be visible
@@ -34,7 +29,45 @@ interface SceneControlInput {
     /**
      * The tool data
      */
-    tool: SceneControlTool;
+    tool: SceneControlToolInput;
 }
 
-export type { SceneControlInput };
+interface SceneControlToolInput {
+    name: string;
+    order?: number;
+    title: string;
+    icon: string;
+    visible?: boolean;
+    toggle?: boolean;
+    active?: boolean;
+    button?: boolean;
+    onChange?: (event?: Event, active?: boolean) => void;
+    toolclip?: ToolclipConfigurationInput;
+}
+
+interface ToolclipConfigurationInput {
+    /** The filename of the toolclip video. */
+    src: string;
+    /** The heading string. */
+    heading: string;
+    /** The items in the toolclip body. */
+    items: ToolclipConfigurationItemInput[];
+}
+
+interface ToolclipConfigurationItemInput {
+    /** A plain paragraph of content for this item. */
+    paragraph?: string;
+    /** A heading for the item. */
+    heading?: string;
+    /** Content for the item. */
+    content?: string;
+    /** If the item is a single key reference, use this instead of content. */
+    reference?: string;
+}
+
+export type {
+    SceneControlInput,
+    SceneControlToolInput,
+    ToolclipConfigurationInput,
+    ToolclipConfigurationItemInput,
+};
