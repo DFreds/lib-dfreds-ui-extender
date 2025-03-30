b297e7cbc2c86f744bbd4446b02757ca58bd1fc9
Derek Fredrickson <dfreds08@gmail.com>
2025-03-30 00:10:16 -0400

    Fix scene control issues due to new format of tools

--- src/ts/scene-controls/create-scene-control.ts
+++ src/ts/scene-controls/create-scene-control.ts
@@ -1,23 +1,45 @@
-import { SceneControlInput } from "./scene-control-input.ts";
+import {
+    SceneControlInput,
+    SceneControlToolInput,
+} from "./scene-control-input.ts";
 
 function createSceneControl(input: SceneControlInput): void {
-    Hooks.on("getSceneControlButtons", (controls: SceneControl[]) => {
-        const { name, position, predicate, tool } = input;
+    Hooks.on(
+        "getSceneControlButtons",
+        // @ts-expect-error Ignore this, this is the correct type
+        (controls: Record<string, SceneControl>) => {
+            const { name, predicate, tool } = input;
 
-        if (predicate && predicate(controls) === false) return;
+            if (predicate && predicate(controls) === false) return;
 
-        const targetControl = controls.find((control) => control.name === name);
+            const targetControl = controls[name];
 
-        if (!targetControl) {
-            throw new Error(`Cannot find target control ${name}`);
-        }
+            if (!targetControl) {
+                throw new Error(`Cannot find target control ${name}`);
+            }
 
-        if (position !== undefined && targetControl.tools.length >= position) {
-            targetControl.tools.splice(position, 0, tool);
-        } else {
-            targetControl.tools.push(tool);
-        }
-    });
+            const targetControlTools = targetControl.tools as unknown as Record<
+                string,
+                SceneControlToolInput
+            >;
+
+            if (tool.order === undefined) {
+                tool.order = Object.keys(targetControl.tools).length + 1; // Foundry starts at 1
+            } else {
+                // Shift existing tools' orders to make space for the new tool
+                Object.values(targetControlTools).forEach((existingTool) => {
+                    const toolOrder = tool.order as number;
+                    const existingOrder = existingTool.order as number;
+
+                    if (existingOrder >= toolOrder) {
+                        existingTool.order = existingOrder + 1;
+                    }
+                });
+            }
+
+            targetControlTools[tool.name] = tool;
+        },
+    );
 }
 
 export { createSceneControl };
