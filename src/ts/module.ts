254abb388f9d58a1771097cb004083b4fec57cdd
Derek Fredrickson <dfreds08@gmail.com>
2025-04-15 00:10:11 -0400

    Revert inclusion of sample

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -1,7 +1,4 @@
 import "../styles/style.scss"; // Keep or else vite will not include this
 import { HooksUiExtender } from "./hooks/index.ts";
-import { mySampleModule } from "./sample.ts";
 
 HooksUiExtender.listen();
-
-mySampleModule();
