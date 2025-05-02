6b546ab38aac84d9d71fd342fb44eaff28e1040b
Derek Fredrickson <dfreds08@gmail.com>
2025-05-02 16:56:36 -0400

    Include sample if in development mode

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -1,4 +1,9 @@
 import "../styles/style.scss"; // Keep or else vite will not include this
 import { HooksUiExtender } from "./hooks/index.ts";
+import { mySampleModule } from "./sample.ts";
 
 HooksUiExtender.listen();
+
+if (BUILD_MODE === "development") {
+    mySampleModule();
+}
