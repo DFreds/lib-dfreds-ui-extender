389f448e600b12522e721f608faa9a755f2b675e
Derek Fredrickson <dfreds08@gmail.com>
2025-08-07 16:49:43 -0400

    Simplify debug hook logic

--- src/ts/hooks/setup.ts
+++ src/ts/hooks/setup.ts
@@ -4,10 +4,7 @@ import { Listener } from "./index.ts";
 const Setup: Listener = {
     listen(): void {
         Hooks.once("setup", () => {
-            if (BUILD_MODE === "development") {
-                // Setup debug mode
-                CONFIG.debug.hooks = true;
-            }
+            CONFIG.debug.hooks = BUILD_MODE === "development";
 
             UiExtender.setup();
         });
